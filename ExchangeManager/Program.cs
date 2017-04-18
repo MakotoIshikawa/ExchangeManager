d358679fc0627533b004602ba0770c854ff4ca03
makoto ishikawa <ishikawm@fsi.co.jp>
2017-04-18 09:46:45 +0900

    メール送信成功

--- ExchangeManager/Program.cs
+++ ExchangeManager/Program.cs
@@ -8,22 +8,21 @@ using Microsoft.Exchange.WebServices.Data;
 namespace ExchangeManager {
 	class Program {
 		static void Main(string[] args) {
-			var url = @"https://kariverification14.sharepoint.com";
 			var username = @"ishikawm@kariverification14.onmicrosoft.com";
 			var password = @"Ishikawam!";
 
 			var service = new ExchangeService(ExchangeVersion.Exchange2013_SP1) {
 				Credentials = new WebCredentials(username, password),
-				UseDefaultCredentials = true,
+				UseDefaultCredentials = false,
 				TraceEnabled = true,
 				TraceFlags = TraceFlags.All,
 			};
 
-			service.AutodiscoverUrl(username, redirectionUrl => {
+			service.AutodiscoverUrl(username, url => {
 				// 検証コールバックのデフォルトは、URLを拒否することです。
 				var result = false;
 
-				var redirectionUri = new Uri(redirectionUrl);
+				var redirectionUri = new Uri(url);
 
 				// リダイレクトURLの内容を検証します。
 				// この単純な検証コールバックでは、HTTPSを使用して認証資格情報を暗号化する場合、
@@ -46,6 +45,33 @@ namespace ExchangeManager {
 			email.ToRecipients.Add(username);
 
 			email.Send();
+
+			// Initialize values for the start and end times, and the number of appointments to retrieve.
+			var startDate = DateTime.Now;
+			var endDate = startDate.AddDays(30);
+			int numAppts = 5;
+
+			// Initialize the calendar folder object with only the folder ID. 
+			var calendar = CalendarFolder.Bind(service, WellKnownFolderName.Calendar, new PropertySet());
+
+			// Set the start and end time and number of appointments to retrieve.
+			var cView = new CalendarView(startDate, endDate, numAppts);
+
+			// Limit the properties returned to the appointment's subject, start time, and end time.
+			cView.PropertySet = new PropertySet(AppointmentSchema.Subject, AppointmentSchema.Start, AppointmentSchema.End);
+
+			// Retrieve a collection of appointments by using the calendar view.
+			FindItemsResults<Appointment> appointments = calendar.FindAppointments(cView);
+
+			Console.WriteLine("\nThe first " + numAppts + " appointments on your calendar from " + startDate.Date.ToShortDateString() +
+							  " to " + endDate.Date.ToShortDateString() + " are: \n");
+
+			foreach (Appointment a in appointments) {
+				Console.Write("Subject: " + a.Subject.ToString() + " ");
+				Console.Write("Start: " + a.Start.ToString() + " ");
+				Console.Write("End: " + a.End.ToString());
+				Console.WriteLine();
+			}
 		}
 	}
 }
